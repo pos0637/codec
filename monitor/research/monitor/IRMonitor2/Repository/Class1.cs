89a7a23b7c1f9094b14f4518216c1323ede86939
pos0637 <pos0637@163.com>
2019-09-23 21:52:07 +0800

    添加新项目。

---
+++ monitor/research/monitor/IRMonitor2/Repository/Class1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class Class1
+    {
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=blogging.db");
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.Entity<Blog>().ToTable("t_test_blog");
+                modelBuilder.Entity<Post>().ToTable("t_test_post");
+            }
+
+            public void Query()
+            {
+                var parameters = new SqliteParameter[] {
+                    new SqliteParameter("Title", "xxx"),
+                    new SqliteParameter("Content", "123456")
+                };
+
+                Database.ExecuteSqlCommand("update t_test_post set Title=@Title Content=@Content where Id>0", parameters);
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Url { get; set; }
+
+            public List<Post> Posts { get; } = new List<Post>();
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public Blog Blog { get; set; }
+        }
+    }
+}
